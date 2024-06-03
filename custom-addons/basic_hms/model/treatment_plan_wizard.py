bd375feaa12de03331f54a54c3691b490ea054bb
ranson.lobo@invenger.com-at-180162959298 <ranson.lobo@invenger.com>
2024-06-03 17:26:45 +0530

    Added Manafacture for dental

--- custom-addons/basic_hms/model/treatment_plan_wizard.py
+++ custom-addons/basic_hms/model/treatment_plan_wizard.py
@@ -37,3 +37,42 @@ class ProjectTask(models.Model):
 
     patient_id = fields.Many2one('medical.patient', string="Patient")
     doctor_id = fields.Many2one('medical.physician', 'Prescribing Doctor')
+
+    product_id = fields.Many2one('product.product', string='Product', required=True)
+    bom_id = fields.Many2one('mrp.bom', string='Bill of Materials', compute='_compute_bom', store=True)
+    quantity = fields.Float(string='Quantity', default=1.0, required=True)
+
+    @api.depends('product_id')
+    def _compute_bom(self):
+        for record in self:
+            if record.product_id:
+                bom = self.env['mrp.bom'].search([('product_tmpl_id', '=', record.product_id.product_tmpl_id.id)],
+                                                 limit=1)
+                record.bom_id = bom if bom else False
+
+    # @api.depends('product_id')
+    # # product_id = fields.Many2one('mrp.bom', string='Product')
+    # bom_ids = fields.One2many('mrp.bom', 'Product')
+    # product_id = fields.One2many('product.product', 'Bill of Material')
+
+    def create_manufacturing_order(self):
+        self.ensure_one()
+        if not self.bom_id:
+            raise UserError('No Bill of Materials found for this product.')
+
+        mo_vals = {
+            'product_id': self.product_id.id,
+            'product_qty': self.quantity,
+            'bom_id': self.bom_id.id,
+            'product_uom_id': self.product_id.uom_id.id,
+        }
+
+        mo = self.env['mrp.production'].create(mo_vals)
+        return {
+            'name': 'Manufacturing Order',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mrp.production',
+            'type': 'ir.actions.act_window',
+            'res_id': mo.id,
+        }
