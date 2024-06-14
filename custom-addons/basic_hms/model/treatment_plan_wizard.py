a0933e26b1ac0adb59228edb1281517191aeb7c4
nsvkumar <nikilsv.kumar@invenger.com>
2024-06-14 18:28:35 +0530

    task_id_issue

--- custom-addons/basic_hms/model/treatment_plan_wizard.py
+++ custom-addons/basic_hms/model/treatment_plan_wizard.py
@@ -33,10 +33,10 @@ class WizardCreateProjectTask(models.TransientModel):
         }
 
 
-# class Project(models.Model):
-#     _inherit = 'project.project'
-#
-#     treatment_stages = fields.Many2many('medical.treatment.stages', string='treatment Stages')
+class Project(models.Model):
+    _inherit = 'project.project'
+
+    treatment_stages = fields.Many2many('medical.treatment.stages', string='treatment Stages')
 
 
 class ProjectTask(models.Model):
@@ -84,67 +84,67 @@ class ProjectTask(models.Model):
             'res_id': mo.id,
         }
 
-    # parent_id = fields.Many2one(
-    #     'project.task',
-    #     string='Parent Task',
-    #     index=True,
-    #     domain="['!', ('id', 'child_of', id)]",
-    #     tracking=True
-    # )
-    #
-    # child_ids = fields.One2many(
-    #     'project.task',
-    #     'parent_id',
-    #     string="Sub-tasks",
-    #     domain="[('recurring_task', '=', False)]"
-    # )
-    # prescription_ids = fields.One2many(
-    #     comodel_name='medical.prescription.order',
-    #     inverse_name='task_id',
-    #     string='Prescriptions'
-    # )
-    # all_child_prescription_ids = fields.One2many(
-    #     comodel_name='medical.prescription.order',
-    #     compute='_compute_all_child_prescription_ids',
-    #     string='All Child Prescriptions',
-    #     store=False
-    # )
-    #
-    # # project_task = fields.One2many(, string='stages')
-    # is_subtask = fields.Boolean(string="Is Subtask", compute="_compute_is_subtask", store=True)
-    #
-    # @api.depends('parent_id')
-    # def _compute_is_subtask(self):
-    #     for task in self:
-    #         task.is_subtask = bool(task.parent_id)
-    #
-    # @api.depends('child_ids.prescription_ids')
-    # def _compute_all_child_prescription_ids(self):
-    #     for task in self:
-    #         all_prescriptions = self.env['medical.prescription.order']
-    #         for child in task.child_ids:
-    #             all_prescriptions |= child.prescription_ids
-    #         task.all_child_prescription_ids = all_prescriptions
-    #
-    # @api.model
-    # def create(self, vals):
-    #     # Check if we are in the context of creating sub-tasks
-    #     if self.env.context.get('creating_sub_tasks'):
-    #         return super(ProjectTask, self).create(vals)
-    #
-    #     # Create the main task
-    #     task = super(ProjectTask, self).create(vals)
-    #
-    #     # Retrieve the treatment_stages from the related project
-    #     if task.project_id.treatment_stages:
-    #         treatment_stages = task.project_id.treatment_stages
-    #         # Create sub-tasks for each treatment_stage with a context flag to avoid recursion
-    #         for stage in treatment_stages:
-    #             self.with_context(creating_sub_tasks=True).create({
-    #                 'name': stage.name,
-    #                 'parent_id': task.id,
-    #                 'project_id': task.project_id.id,
-    #                 'stage_id': stage.id,
-    #             })
-    #
-    #     return task
+    parent_id = fields.Many2one(
+        'project.task',
+        string='Parent Task',
+        index=True,
+        domain="['!', ('id', 'child_of', id)]",
+        tracking=True
+    )
+
+    child_ids = fields.One2many(
+        'project.task',
+        'parent_id',
+        string="Sub-tasks",
+        domain="[('recurring_task', '=', False)]"
+    )
+    prescription_ids = fields.One2many(
+        comodel_name='medical.prescription.order',
+        inverse_name='task_id',
+        string='Prescriptions'
+    )
+    all_child_prescription_ids = fields.One2many(
+        comodel_name='medical.prescription.order',
+        compute='_compute_all_child_prescription_ids',
+        string='All Child Prescriptions',
+        store=False
+    )
+
+    # project_task = fields.One2many(, string='stages')
+    is_subtask = fields.Boolean(string="Is Subtask", compute="_compute_is_subtask", store=True)
+
+    @api.depends('parent_id')
+    def _compute_is_subtask(self):
+        for task in self:
+            task.is_subtask = bool(task.parent_id)
+
+    @api.depends('child_ids.prescription_ids')
+    def _compute_all_child_prescription_ids(self):
+        for task in self:
+            all_prescriptions = self.env['medical.prescription.order']
+            for child in task.child_ids:
+                all_prescriptions |= child.prescription_ids
+            task.all_child_prescription_ids = all_prescriptions
+
+    @api.model
+    def create(self, vals):
+        # Check if we are in the context of creating sub-tasks
+        if self.env.context.get('creating_sub_tasks'):
+            return super(ProjectTask, self).create(vals)
+
+        # Create the main task
+        task = super(ProjectTask, self).create(vals)
+
+        # Retrieve the treatment_stages from the related project
+        if task.project_id.treatment_stages:
+            treatment_stages = task.project_id.treatment_stages
+            # Create sub-tasks for each treatment_stage with a context flag to avoid recursion
+            for stage in treatment_stages:
+                self.with_context(creating_sub_tasks=True).create({
+                    'name': stage.name,
+                    'parent_id': task.id,
+                    'project_id': task.project_id.id,
+                    'stage_id': stage.id,
+                })
+
+        return task
