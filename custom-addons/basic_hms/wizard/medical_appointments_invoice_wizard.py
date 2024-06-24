69f5f0cfe638866883526ad926ace6b462144a55
nsvkumar <nikilsv.kumar@invenger.com>
2024-06-24 16:56:29 +0530

    indeIndentationError

--- custom-addons/basic_hms/wizard/medical_appointments_invoice_wizard.py
+++ custom-addons/basic_hms/wizard/medical_appointments_invoice_wizard.py
@@ -44,7 +44,6 @@ class medical_appointments_invoice_wizard(models.TransientModel):
         if lab_req.consultations_id.id:
             invoice_line_account_id = lab_req.consultations_id.property_account_income_id.id or lab_req.consultations_id.categ_id.property_account_income_categ_id.id or False
         if not invoice_line_account_id:
-            inc_acc = ir_property_obj.get('property_account_income_categ_id', 'product.category')
             inc_acc = ir_property_obj.get_by_record('property_account_income_categ_id', self.env['product.category'])
             if inc_acc:
                 invoice_line_account_id = inc_acc.id
