917b2ffb2d1b29bddbd58772b205a9d08677ccbd
ranson.lobo@invenger.com-at-180162959298 <ranson.lobo@invenger.com>
2024-06-26 14:53:22 +0530

    DAshboard fix

--- custom-addons/odoo_dynamic_dashboard/static/src/js/DynamicDashboard.js
+++ custom-addons/odoo_dynamic_dashboard/static/src/js/DynamicDashboard.js
@@ -63,4 +63,4 @@
 
  DynamicDashboard.template = "owl.dynamic_dashboard";
  DynamicDashboard.components = { DynamicDashboardTile, DynamicDashboardChart };
-registry.category("actions").add("owl.dynamic_dashboard", DynamicDashboard);
+registry.category("actions").remove("owl.dynamic_dashboard", DynamicDashboard);
