b4b2c813befb0d0300c601c16b1d8b8464fc38e8
nsvkumar <nikilsv.kumar@invenger.com>
2024-06-26 14:36:19 +0530

    invoice issue

--- custom-addons/basic_hms/wizard/medical_appointments_invoice_wizard.py
+++ custom-addons/basic_hms/wizard/medical_appointments_invoice_wizard.py
@@ -1,36 +1,46 @@
-# -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
-
+import logging
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 from datetime import date, datetime
 
-class medical_appointments_invoice_wizard(models.TransientModel):
+_logger = logging.getLogger(__name__)
+
+
+class MedicalAppointmentsInvoiceWizard(models.TransientModel):
     _name = "medical.appointments.invoice.wizard"
-    _description = 'medical appointments invoice wizard'
+    _description = 'Medical Appointments Invoice Wizard'
 
     def create_invoice(self):
-        active_ids = self._context.get('active_ids')
-        list_of_ids = []
+        active_ids = self._context.get('active_ids', [])
+        if not active_ids:
+            raise UserError(_('No active appointments selected.'))
+
         lab_req_obj = self.env['medical.appointment']
         account_invoice_obj = self.env['account.move']
         ir_property_obj = self.env['ir.property']
-        
+        list_of_ids = []
+
         for active_id in active_ids:
             lab_req = lab_req_obj.browse(active_id)
-            lab_req.validity_status = 'invoice'
-            
+            _logger.info('Processing appointment: %s', lab_req.name)
+
+            if lab_req.validity_status == 'invoice':
+                continue
+
             if lab_req.is_invoiced:
-                raise UserError(_('Already Invoiced.'))
+                raise UserError(_('Appointment %s is already invoiced.') % lab_req.name)
 
             if not lab_req.no_invoice:
-                sale_journals = self.env['account.journal'].search([('type', '=', 'sale')])
+                sale_journals = self.env['account.journal'].search([('type', '=', 'sale')], limit=1)
+                if not sale_journals:
+                    raise UserError(_('No sale journals found.'))
+
                 invoice_vals = {
                     'name': self.env['ir.sequence'].next_by_code('medical_app_inv_seq'),
                     'invoice_origin': lab_req.name or '',
                     'move_type': 'out_invoice',
                     'ref': False,
-                    'partner_id': lab_req.patient_id.patient_id.id or False,
+                    'partner_id': lab_req.patient_id.patient_id.id,
                     'partner_shipping_id': lab_req.patient_id.patient_id.id,
                     'currency_id': lab_req.patient_id.patient_id.currency_id.id,
                     'invoice_payment_term_id': False,
@@ -39,29 +49,24 @@ class medical_appointments_invoice_wizard(models.TransientModel):
                     'invoice_date': date.today(),
                     'journal_id': sale_journals.id,
                 }
-                res = account_invoice_obj.create(invoice_vals)
-                invoice_line_account_id = False
-                
-                if lab_req.consultations_id.id:
-                    invoice_line_account_id = lab_req.consultations_id.property_account_income_id.id or lab_req.consultations_id.categ_id.property_account_income_categ_id.id or False
-                
-                if not invoice_line_account_id:
-                    inc_acc = ir_property_obj.get('property_account_income_categ_id', 'product.category')
+                invoice = account_invoice_obj.create(invoice_vals)
+                invoice_line_account_id = lab_req.consultations_id.property_account_income_id.id or lab_req.consultations_id.categ_id.property_account_income_categ_id.id
 
+                if not invoice_line_account_id:
+                    _logger.info('Fetching income account for category: %s', lab_req.consultations_id.categ_id.name)
+                    inc_acc = ir_property_obj.get_by_record('property_account_income_categ_id',
+                                                            lab_req.consultations_id.categ_id)
                     if inc_acc:
                         invoice_line_account_id = inc_acc.id
-                    
+
                     if not invoice_line_account_id:
                         raise UserError(
-                            _('There is no income account defined for this product: "%s". You may have to install a chart of account from the Accounting app, settings menu.') %
-                            (lab_req.consultations_id.name,)
-                        )
+                            _('No income account defined for product: "%s".') % lab_req.consultations_id.name)
 
-                tax_ids = []
                 taxes = lab_req.consultations_id.taxes_id.filtered(
-                    lambda r: not lab_req.consultations_id.company_id or r.company_id == lab_req.consultations_id.company_id
+                    lambda
+                        r: not lab_req.consultations_id.company_id or r.company_id == lab_req.consultations_id.company_id
                 )
-                tax_ids = taxes.ids
 
                 invoice_line_vals = {
                     'name': lab_req.consultations_id.name or '',
@@ -70,30 +75,28 @@ class medical_appointments_invoice_wizard(models.TransientModel):
                     'product_uom_id': lab_req.consultations_id.uom_id.id,
                     'quantity': 1,
                     'product_id': lab_req.consultations_id.id,
+                    'tax_ids': [(6, 0, taxes.ids)],
                 }
 
-                res.write({'invoice_line_ids': [(0, 0, invoice_line_vals)]})
+                invoice.write({'invoice_line_ids': [(0, 0, invoice_line_vals)]})
+                list_of_ids.append(invoice.id)
+                lab_req.write({'is_invoiced': True, 'validity_status': 'invoice'})
 
-                list_of_ids.append(res.id)
-                lab_req.write({'is_invoiced': True})
-        
         if list_of_ids:
-            imd = self.env['ir.model.data']
             action = self.env.ref('account.action_move_out_invoice_type')
-            list_view_id = imd.sudo()._xmlid_to_res_id('account.view_invoice_tree')
-            form_view_id = imd.sudo()._xmlid_to_res_id('account.view_move_form')
             result = {
                 'name': action.name,
                 'help': action.help,
                 'type': action.type,
-                'views': [[list_view_id, 'tree'], [form_view_id, 'form']],
+                'views': [[self.env.ref('account.view_invoice_tree').id, 'tree'],
+                          [self.env.ref('account.view_move_form').id, 'form']],
                 'target': action.target,
                 'context': action.context,
                 'res_model': action.res_model,
-                'domain': "[('id','in',%s)]" % list_of_ids
+                'domain': [('id', 'in', list_of_ids)]
             }
             return result
         else:
-            raise UserError(_('The Appointment is invoice exempt'))
+            raise UserError(_('The selected appointments are invoice exempt.'))
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
