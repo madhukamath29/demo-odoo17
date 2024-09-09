c0120de6ea746faf90af09b412449f38aee335fa
madhu <madhukamath9@gmail.com>
2024-09-09 17:51:48 +0530

    updated files

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-rsync -vrah /tmp/codedeploy/ /opt/odoo17/odoo17/
+rsync -vrah /temp/enterprise_addon /opt/odoo17/odoo17/
 #rm -rf "/tmp/codedeploy"
 echo "Code Deployed to /opt/odoo17/odoo17/"
 
