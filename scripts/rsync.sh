737495635dc6873dfffd238ef601059e411682f1
madhu <madhukamath9@gmail.com>
2024-09-09 15:10:44 +0530

    test-3

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-rsync -vrah /tmp/codedeploy1/ /opt/odoo17/odoo17/
-rm -rf "/tmp/codedeploy1"
+rsync -vrah /tmp/codedeploy/ /opt/odoo17/odoo17/
+rm -rf "/tmp/codedeploy"
 echo "Code Deployed to /opt/odoo17/odoo17/"
 
