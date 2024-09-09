f5b7a45ac712127b8700225514a526b72f5d9a78
madhu <madhukamath9@gmail.com>
2024-09-09 12:05:51 +0530

    merged_code

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
-rsync -vrah /tmp/codedeploy/ /opt/odoo17/odoo17/
+rsync -vrah /tmp/codedeploy/merged_code /opt/odoo17/odoo17/
 rm -rf "/tmp/codedeploy"
-echo "Code Deployed to /opt/odoo17/odoo17/"
+echo "Code Deployed to /opt/odoo17/odoo17/merged_code"
+
