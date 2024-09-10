92561bd63015004c149242e861c9506b68832b34
madhu <madhukamath9@gmail.com>
2024-09-10 10:25:18 +0530

    updated script..

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
-rsync -vrah /temp/enterprise_addon /opt/odoo17/odoo17/
-rm -rf "/temp/enterprise_addon"
+
+# Use rsync to copy the files
+rsync -vrah /temp/enterprise_addon/ /opt/odoo17/odoo17/
+
+# Remove the temporary directory
+rm -rf /temp/enterprise_addon
+
 echo "Code Deployed to /opt/odoo17/odoo17/"
 
