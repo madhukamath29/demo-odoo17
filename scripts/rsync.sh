21568a158bc9614be95af102f81fd431159e1ff8
madhu <madhukamath9@gmail.com>
2024-09-13 16:39:41 +0530

    test

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  set -e
 
- Sync files from the source directory to the destination
+ #Sync files from the source directory to the destination
 rsync -vrah /opt/enterprise_addons/ /opt/odoo17/odoo17/
 
 Optionally remove the source directory if no longer needed
