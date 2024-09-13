be32182e0f84abf07685714c487b8d9611876223
madhu <madhukamath9@gmail.com>
2024-09-13 16:32:23 +0530

    test1

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# set -e
+ set -e
 
-#  Sync files from the source directory to the destination
-# rsync -vrah /temp/test/ /opt/odoo17/odoo17/
+ Sync files from the source directory to the destination
+rsync -vrah /opt/enterprise_addons/ /opt/odoo17/odoo17/
 
-# Optionally remove the source directory if no longer needed
-# rm -rf /temp/test 
+Optionally remove the source directory if no longer needed
+rm -rf /opt/enterprise_addons
 
-# echo "Code Deployed to /opt/odoo17/odoo17/"
+echo "Code Deployed to /opt/odoo17/odoo17/"
 
 
 
